9ddfe146e35ed8c3c36b52e6a47965965d2b9d43
carlsonclaire <cec9702@stern.nyu.edu>
2021-07-27 12:31:38 -0500

    Initial setup

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,5 +1,32 @@
-#Setup
 
+
+# app / robo_advisor.py
+
+import requests
+import json #> string to dict
+
+# INFO INPUTS
+
+requests_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo"
+response = requests.get(requests_url)
+#print(type(response))
+#print(response.status_code)
+#print(response.text) #know this is a string
+
+parsed_response = json.loads(response.text) #this gets us data in dictionary form
+breakpoint()
+
+
+#url =
+#r = requests.get(url)
+#data = r.json()
+
+#print(data)
+
+# INFO OUTPUTS
+
+
+#Setup
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
