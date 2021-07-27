aa03d79dc62d76b1987cd79229ee7a04165762ce
carlsonclaire <cec9702@stern.nyu.edu>
2021-07-27 12:43:32 -0500

    updates to latest response

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -14,7 +14,9 @@ response = requests.get(requests_url)
 #print(response.text) #know this is a string
 
 parsed_response = json.loads(response.text) #this gets us data in dictionary form
-breakpoint()
+
+last_refreshed = parsed_response["Meta Data"] ["3. Last Refreshed"]
+#breakpoint()
 
 
 #url =
@@ -22,6 +24,7 @@ breakpoint()
 #data = r.json()
 
 #print(data)
+
 
 # INFO OUTPUTS
 
@@ -33,7 +36,7 @@ print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
+print(f"LATEST DAY: {last_refreshed}")
 print("LATEST CLOSE: $100,000.00")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
